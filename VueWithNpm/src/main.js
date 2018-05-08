bcd098c97cc412a18b623bd3922d64a5d05eb429
xun.wu@oracle.com <xun.wu@oracle.com>
2018-05-08 17:15:02 +0800

    v-if and v-for

--- VueWithNpm/src/main.js
+++ VueWithNpm/src/main.js
@@ -32,6 +32,11 @@
                 { id: 1, text: '奶酪' },
                 { id: 2, text: '随便其它什么人吃的东西' }
             ],
+            groceryListCopy: [
+                { id: 0, text: '蔬菜' },
+                { id: 1, text: '奶酪' },
+                { id: 2, text: '随便其它什么人吃的东西' }
+            ],
             fw: 'bold',
             stObj: {
                 fontWeight: 'bold',
@@ -39,12 +44,41 @@
             },
             stObj1: {
                 color: 'red'
+            },
+            vIfFlg: true,
+            vForObj: {
+                item1: "item1Value",
+                item2: "item2Value",
+                item3: "item3Value",
+                item4: "item4Value",
             }
         },
         methods: {
             clickFunc: function () {
                 this.stObj.fontWeight = '';
                 this.stObj.fontSize = '1em';
+            },
+            vIfYesClickFunc: function (event) {
+                this.vIfFlg = false;
+            },
+            vIfNoClickFunc: function (event) {
+                this.vIfFlg = true;
+            },
+            reversalArr: function (event) {
+                this.groceryList.reverse();
+            },
+            reversalArrCopy: function (event) {
+                this.groceryListCopy.reverse();
+            },
+            removeItemArr: function (event) {
+                this.groceryListCopy.pop();
+            },
+            addItemArr: function () {
+                this.groceryListCopy.push({ id: 3, text: 'new item' });
+            },
+            addItemForObj: function () {
+                var keyName = Object.keys(this.vForObj).length + 1;
+                Vue.set(this.vForObj, 'item' + keyName, 'item' + keyName + 'Value')
             }
         }
     });
