f58f325b94f2c160ebbb916734041079840c81cd
xun.wu@oracle.com <xun.wu@oracle.com>
2018-05-08 10:20:56 +0800

    test

--- VueWithNpm/src/main.js
+++ VueWithNpm/src/main.js
@@ -12,7 +12,7 @@
 
     Vue.component('todo-item', {
         // todo-item 组件现在接受一个
-        // "prop"，类似于一个自定义特性。
+        // "prop"，类似于一个自定义特性。11
         // 这个 prop 名为 todo。
         props: ['todo'],
         template: '<li>{{ todo.text }}</li>'
