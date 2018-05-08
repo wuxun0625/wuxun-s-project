81d9e8841c37bcf49ac555a58a74e00111bfd6a5
xun.wu@oracle.com <xun.wu@oracle.com>
2018-05-08 09:39:45 +0800

    commit form working laptop

--- VueWithNpm/src/main.js
+++ VueWithNpm/src/main.js
@@ -13,7 +13,7 @@
     Vue.component('todo-item', {
         // todo-item 组件现在接受一个
         // "prop"，类似于一个自定义特性。
-        // 这个 prop 名为 todo。1122
+        // 这个 prop 名为 todo。
         props: ['todo'],
         template: '<li>{{ todo.text }}</li>'
     })
